c4c836eeaaa580ea5bf1b1264bf165212204b2a7
Bilaljanir <bilaljanir@gmail.com>
2023-10-30 09:55:23 +0100

    Checks if the grille is full

--- src/Main.java
+++ src/Main.java
@@ -89,6 +89,13 @@ public class Main {
     }
 
     private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != 'X' && board[i][j] != 'O') {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
